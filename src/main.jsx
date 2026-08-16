2ade6afc3c66e5f54eaab6c02b220c14dd54dc0f
danielhenr86-ui <danielhenr86@gmail.com>
2026-08-15 23:48:13 -0300

    Mount synced appearance control

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MetonNext from "./MetonNext.jsx";
 import InstallPrompt from "./InstallPrompt.jsx";
+import ThemeControl from "./ThemeControl.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MetonNext />
+    <ThemeControl />
     <InstallPrompt />
   </React.StrictMode>
 );
